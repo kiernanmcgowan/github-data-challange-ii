639f440a9034b7c0ba2559b79dae5322518340ce
Kiernan McGowan <timmcgowan625@gmail.com>
2013-04-27 23:26:27 -0500

    finished python import script

--- utils/import.py
+++ utils/import.py
@@ -1,24 +1,31 @@
-# python import script
+# python import to postgres script
 import gzip
 import json
+import os
 import psycopg2 as db
 
-insertQuery = db.prepare('INSERT INTO logdata(repo, hour, event, stars, payload) VALUES($1, $2, $3, $4, $5)')
+conn = db.connect("dbname='postgres' user='data' host='localhost' password='moardata'")
+insertQuery ='INSERT INTO logdata(repo, hour, event, stars, payload) VALUES(%s, %s, %s, %s, %s)'
+
+cur = conn.cursor()
 
 
 def storeInPostgres(file, fName, num):
     f = gzip.open(file, 'rb')
     fc = f.read()
     obj = json.loads(fc)
-    del obj['undefined']
+
+    # remove bad repos
+    if obj.get('undefined', False):
+        del obj['undefined']
 
     reposToInsert = []
-    for repoId, repoObj in obj:
+    for repoId, repoObj in obj.iteritems():
         # scan for stars (and it is always stars)
         didBreak = False
-        for eventType, eventObj in repoObj:
+        for eventType, eventObj in repoObj.iteritems():
             for event in eventObj:
-                if event.repo.watchers >= num:
+                if event['repo'].get('watchers', -1) >= num:
                     reposToInsert.append(repoId)
                     didBreak = True
                     break
@@ -32,6 +39,17 @@ def storeInPostgres(file, fName, num):
 
     for repoId in reposToInsert:
         repo = obj[repoId]
-        for eventType, eventArr in repo:
+        for eventType, eventArr in repo.iteritems():
             for event in eventArr:
-                insertQuery(repoId, timeStamp, eventType, event['repo']['watchers'], json.dumps(event))
+                cur.execute(insertQuery, (repoId, timeStamp, eventType, event['repo']['watchers'], json.dumps(event)))
+                conn.commit()
+
+
+
+def getFiles(dir, num):
+    print dir
+    for files in os.listdir(dir):
+        print files
+        storeInPostgres(os.path.join(dir, files), files, num)
+
+getFiles('../reformat', 5)
